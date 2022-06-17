dff4ad24d5f242971a8368aef73142107161a274
Rimi Mukherjee <107538210+RimiMukherjeeIBM@users.noreply.github.com>
2022-06-17 18:10:23 +0530

    Modification

--- src/main/java/com/ibm/cloud/demo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/ibm/cloud/demo/configuration/SwaggerConfiguration.java
@@ -8,18 +8,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-/*import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;*/
-/*
-@EnableSwagger2*/
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @Configuration
 public class SwaggerConfiguration {
-/*
+
     @Value("${swagger.api-info.title}")
     private String title;
     @Value("${swagger.api-info.description}")
@@ -45,6 +45,6 @@ public class SwaggerConfiguration {
 
     private ApiInfo getApiInfo() {
         return new ApiInfo(title, description, version, tos, new Contact(contactName, "", contactEmail), license, licenseUrl, Collections.EMPTY_LIST);
-    }*/
+    }
 }
 
